e2afdc137fcaa4ed05a77f84bc814f028ffe254a
Marko Žerajić <zerajima@gmail.com>
2023-12-17 22:20:38 +0100

    Upgrade target framework

---
+++ Day18/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Day18;
+
+internal static class Program
+{
+    private const string TestInputPath = "TestInput.txt";
+    private const string RealInputPath = "RealInput.txt";
+   
+    static void Main(string[] args)
+    {
+        Task1(TestInputPath);
+        Task2(TestInputPath);
+    }
+
+    private static void Task1(string inputPath)
+    {
+        var score = 0;
+        var input = File.ReadAllLines(inputPath);
+        
+        foreach (var line in input)
+        {
+        }
+
+
+        Console.WriteLine(score);
+    }
+    
+    private static void Task2(string inputPath)
+    {
+        var score = 0;
+        var input = File.ReadAllLines(inputPath);
+        
+        foreach (var line in input)
+        {
+        }
+
+
+        Console.WriteLine(score);   
+    }
+}
