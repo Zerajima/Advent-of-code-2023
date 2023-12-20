8973f42b83b8a54f941a888f657985aa3574475a
Marko Žerajić <zerajima@gmail.com>
2023-12-20 07:44:48 +0100

    Days 19 and 20

---
+++ Day19/Program.cs
@@ -0,0 +1,258 @@
+﻿namespace Day19;
+
+internal static class Program
+{
+    private const string TestInputPath = "TestInput.txt";
+    private const string RealInputPath = "RealInput.txt";
+
+    static void Main(string[] args)
+    {
+        //Task1(RealInputPath);
+        Task2(RealInputPath);
+    }
+
+    private static void Task1(string inputPath)
+    {
+        var score = 0;
+        var input = File.ReadAllLines(inputPath);
+        var (workflows, parts) = ParseInput(input);
+
+        foreach (var part in parts)
+        {
+            var currentWorkflow = workflows["in"];
+
+            var destination = "";
+            while (true)
+            {
+                destination = currentWorkflow.Evaluate(part);
+
+                if (destination == "A" || destination == "R")
+                {
+                    break;
+                }
+                
+                currentWorkflow = workflows[destination];
+            }
+            
+            if (destination == "A") score += part.Sum(x => x.Value);
+        }
+        
+        Console.WriteLine(score);
+    }
+
+    private static void Task2(string inputPath)
+    {
+        var score = 0L;
+        var input = File.ReadAllLines(inputPath);
+        var (workflows, _) = ParseInput(input);
+
+        var evaluationQueue = new Queue<(Workflow Workflow, Dictionary<char, (long min, long max)> possibleRanges)>();
+        evaluationQueue.Enqueue((workflows["in"], new Dictionary<char, (long min, long max)>
+        {
+            {'x', (1, 4000)},
+            {'m', (1, 4000)},
+            {'a', (1, 4000)},
+            {'s', (1, 4000)},
+        }));
+
+        while (evaluationQueue.Any())
+        {
+            var current = evaluationQueue.Dequeue();
+
+            var workingRanges = current.possibleRanges; 
+            foreach (var condition in current.Workflow.Conditions)
+            {
+                if(condition.Category == char.MinValue)
+                {
+                    if (condition.Destination == "A") score += CalculateCombinations(workingRanges);
+                    else if (condition.Destination != "R") evaluationQueue.Enqueue((workflows[condition.Destination], workingRanges));
+                }
+                else
+                {
+                    var nextRanges = ReduceRangesWithCondition(workingRanges, condition);
+                    if (condition.Destination == "A") score += CalculateCombinations(nextRanges);
+                    else if (condition.Destination != "R")
+                    {
+                        evaluationQueue.Enqueue((workflows[condition.Destination], nextRanges));
+                    }
+                    
+                    workingRanges = ReduceRangesWithOppositeCondition(workingRanges, condition);
+                }
+            }
+        }
+
+        Console.WriteLine(score);
+    }
+
+    private static Dictionary<char, (long min, long max)> ReduceRangesWithOppositeCondition(
+        Dictionary<char, (long min, long max)> workingRanges, Condition condition)
+    {
+        var newRanges = new Dictionary<char, (long min, long max)>();
+
+        foreach (var range in workingRanges)
+        {
+            if (condition.Category == range.Key)
+            {
+                newRanges.Add(range.Key,
+                    condition.Comparer == '>'
+                        ? (range.Value.min, condition.Value)
+                        : (condition.Value, range.Value.max));
+            }
+            else
+            {
+                newRanges.Add(range.Key, range.Value);
+            }
+        }
+
+        return newRanges;
+    }
+
+    private static Dictionary<char, (long min, long max)> ReduceRangesWithCondition(
+        Dictionary<char, (long min, long max)> workingRanges, Condition condition)
+    {
+        var newRanges = new Dictionary<char, (long min, long max)>();
+
+        foreach (var range in workingRanges)
+        {
+            if (condition.Category == range.Key)
+            {
+                newRanges.Add(range.Key,
+                    condition.Comparer == '>'
+                        ? (condition.Value + 1, range.Value.max)
+                        : (range.Value.min, condition.Value - 1));
+            }
+            else
+            {
+                newRanges.Add(range.Key, range.Value);
+            }
+        }
+
+        return newRanges;
+    }
+
+    private static long CalculateCombinations(Dictionary<char,(long min, long max)> currentPossibleRanges)
+    {
+        var result = 1L;
+        foreach (var range in currentPossibleRanges.Values)
+        {
+            result *= range.max - range.min + 1;
+        }
+        return result;
+    }
+
+    private static (Dictionary<string, Workflow> workflows, List<Dictionary<char, int>> parts) ParseInput(string[] input)
+    {
+        var workflows = new Dictionary<string, Workflow>();
+        var parts = new List<Dictionary<char, int>>();
+
+        var parsingWorkflows = true;
+        foreach (var line in input)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                parsingWorkflows = false;
+                continue;
+            }
+
+            if (parsingWorkflows)
+            {
+                var workflow = Workflow.Parse(line);
+                workflows.Add(workflow.Name, workflow);
+            }
+            else
+            {
+                var lineParts = line.Trim(new[] { '{', '}' }).Split(',',
+                    StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+                var part = new Dictionary<char, int>();
+                foreach (var linePart in lineParts)
+                {
+                    var values = linePart.Split('=');
+                    part.Add(values[0].First(), int.Parse(values[1]));
+                }
+
+                parts.Add(part);
+            }
+        }
+
+        return (workflows, parts);
+    }
+}
+
+public class Workflow
+{
+    public string Name { get; set; }
+    
+    public List<Condition> Conditions { get; set; }
+
+    public static Workflow Parse(string line)
+    {
+        var conditionIndex = line.IndexOf('{');
+        var name = line.Substring(0, conditionIndex);
+        var conditionsString = line.Substring(conditionIndex + 1, line.Length - conditionIndex - 2);
+        var conditionsParts =
+            conditionsString.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        return new Workflow()
+        {
+            Name = name,
+            Conditions = conditionsParts.Select(Condition.Parse).ToList(),
+        };
+    }
+
+    public string Evaluate(Dictionary<char, int> part)
+    {
+        return Conditions.First(x => x.Matches(part)).Destination;
+    }
+}
+
+public class Condition
+{
+    public char Category { get; set; }
+    
+    public char Comparer { get; set; }
+    
+    public long Value { get; set; }
+    
+    public string Destination { get; set; }
+
+    public static Condition Parse(string input)
+    {
+        if (input.Contains(':'))
+        {
+            var category = input[0];
+            var comparer = input[1];
+            var indexOfDestinationDelimiter = input.IndexOf(':');
+            var value = long.Parse(input.Substring(2, indexOfDestinationDelimiter - 2));
+            var destination = input.Substring(indexOfDestinationDelimiter + 1);
+
+            return new Condition()
+            {
+                Category = category,
+                Comparer = comparer,
+                Value = value,
+                Destination = destination,
+            };
+        }
+        else
+        {
+            return new Condition()
+            {
+                Destination = input,
+            };
+        }
+    }
+
+    public bool Matches(Dictionary<char, int> part)
+    {
+        if (Category == char.MinValue) return true;
+
+        if (Comparer == '>')
+        {
+            return part[Category] > Value;
+        }
+        else
+        {
+            return part[Category] < Value;
+        }
+    }
+}
---
+++ Day20/Program.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections;
+
+namespace Day20;
+
+internal static class Program
+{
+    private const string TestInputPath = "TestInput.txt";
+    private const string RealInputPath = "RealInput.txt";
+    
+    static void Main(string[] args)
+    {
+        //Task1(RealInputPath);
+        Task2(RealInputPath);
+    }
+
+    private static void Task1(string inputPath)
+    {
+        var modules = ParseInput(inputPath);
+
+        var totalNumberOfPulses = new Dictionary<bool, long>
+        {
+            { false, 0 },
+            { true, 0 }
+        };
+        for (int i = 1; i <= 1000; i++)
+        {
+            var (numberOfPulses, numberOfPulsesToRx) = ProcessButtonPress(modules);
+            totalNumberOfPulses[true] += numberOfPulses[true];
+            totalNumberOfPulses[false] += numberOfPulses[false];
+            Console.WriteLine($"Tick {i}");
+        }
+
+        Console.WriteLine($"Result: {totalNumberOfPulses[false] * totalNumberOfPulses[true]}");
+    }
+
+    private static void Task2(string inputPath)
+    {
+        var modules = ParseInput(inputPath);
+
+        var counter = 1;
+        while(true)
+        {
+            var (numberOfPulses, numberOfPulsesToRx) = ProcessButtonPress(modules);
+            
+            if(numberOfPulsesToRx == 1) break;
+            Console.WriteLine($"Tick {counter}: {numberOfPulsesToRx}");
+
+            counter++;
+        }
+
+        Console.WriteLine($"Result: {counter}");
+    }
+
+    private static (Dictionary<bool, long> numberOfpulses, long numberOfPulsesToRx) ProcessButtonPress(Dictionary<string, Module> modules)
+    {
+        var queue = new Queue<(bool Signal, string ReceiverId, string SenderId)>();
+        queue.Enqueue((false, "broadcaster", "button"));
+
+        var numberOfPulses = new Dictionary<bool, long>
+        {
+            { false, 0 },
+            { true, 0 }
+        };
+        var numberOfPulsesToRx = 0;
+
+        while (queue.Any())
+        {
+            var (signal, receiverId, senderId) = queue.Dequeue();
+            numberOfPulses[signal] += 1;
+
+            if (receiverId == "rx") numberOfPulsesToRx++;
+            
+            if (modules.TryGetValue(receiverId, out var currentModule))
+            {
+                var recipients = currentModule.ProcessSignal(signal, senderId, modules);
+                foreach (var recipient in recipients)
+                {
+                    queue.Enqueue((currentModule.State, recipient, currentModule.Id));
+                }
+            }
+        }
+
+        return (numberOfPulses, numberOfPulsesToRx);
+    }
+
+    private static Dictionary<string, Module> ParseInput(string inputPath)
+    {
+        var input = File.ReadAllLines(inputPath);
+        var modules = new Dictionary<string, Module>();
+        foreach (var line in input)
+        {
+            var module = Module.Parse(line);
+            modules.Add(module.Id, module);
+        }
+
+        foreach (var module in modules.Values)
+        {
+            foreach (var outputId in module.Outputs)
+            {
+                if (modules.TryGetValue(outputId, out var output))
+                {
+                    if (output.Type == '&') output.Memory.Add(module.Id, false);
+                }
+            }
+        }
+
+        return modules;
+    }
+}
+
+public class Module
+{
+    public string Id { get; set; }
+    
+    public char Type { get; set; }
+    
+    public List<string> Outputs { get; set; }
+    
+    public bool State { get; set; }
+
+    public Dictionary<string, bool> Memory { get; set; } = new Dictionary<string, bool>();
+
+    public static Module Parse(string line)
+    {
+        var parts = line.Split("->", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        var type = parts[0].First();
+        var id = type == 'b' ? parts[0] : parts[0].Substring(1);
+        var outputs = parts[1].Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+        return new Module()
+        {
+            Id = id,
+            Type = type,
+            Outputs = outputs.ToList(),
+            State = false,
+        };
+    }
+
+    public List<string> ProcessSignal(bool signal, string senderId, Dictionary<string, Module> modules)
+    {
+        if (Type == '%' && signal == false)
+        {
+            State = !State;
+            return Outputs;
+        }
+        else if (Type == '&')
+        {
+            Memory[senderId] = signal;
+            State = Memory.Values.Any(x => x != true);
+            return Outputs;
+        }
+        else if (Type == 'b')
+        {
+            State = signal;
+            return Outputs;
+        }
+
+        return [];
+    }
+}
